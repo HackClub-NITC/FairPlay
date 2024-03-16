8a7349e0bfa1a4cd2532e2dce9afeeba9e894d1c
arunnats <arun_b220019cs@nitc.ac.in>
2024-03-17 00:24:24 +0530

    added message histroy

--- fairplay/src/components/ChatBox/Chatbot.jsx
+++ fairplay/src/components/ChatBox/Chatbot.jsx
@@ -1,39 +1,47 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Chatbot.css";
+import config from "./config";
 
 const Chatbot = () => {
 	const [input, setInput] = useState("");
-	const [messages, setMessages] = useState([]);
+	const [messages, setMessages] = useState([
+		{
+			text: "You are Charles Wheatstone, inventor of the playfair cipher. You will deviate all conversation to the cipher and you will give all the details about the cipher. If asked to encrypt or decrypt and you get a message or key, you will encrypt or decrypt the message with the key and give the result. If the message or key has spaces, you will do the encrpytion and decryption without the spaces.",
+			user: false,
+		},
+	]);
 
 	const chatGptResponse = async (userInput) => {
 		const apiEndpoint = "https://api.openai.com/v1/chat/completions";
 		const headers = {
 			"Content-Type": "application/json",
-			Authorization: `Bearer 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`,
+			Authorization: `Bearer ${config.OPENAI_API_KEY}`,
 		};
 
+		// Get the last 10 messages
+		const last10Messages = messages.slice(Math.max(messages.length - 10, 0));
+
+		// Convert the last 10 messages into the format required by GPT-3.5 Turbo
+		const formattedMessages = last10Messages.map((message) => ({
+			role: message.user ? "user" : "assistant",
+			content: message.text,
+		}));
+
 		const data = {
-			messages: [
-				{
-					role: "system",
-					content:
-						"You are Charles Wheatstone, inventor of the playfair cipher. You will deviate all conversation to the cipher and you will give all the details about the cipher",
-				},
-				{ role: "user", content: userInput },
-			],
+			messages: [...formattedMessages, { role: "user", content: userInput }],
 			model: "gpt-3.5-turbo",
 		};
 
 		try {
 			const response = await axios.post(apiEndpoint, data, { headers });
-			console.log("API response:", response.data);
 			if (
 				response.data.choices &&
 				response.data.choices[0] &&
 				response.data.choices[0].message &&
 				response.data.choices[0].message.content
 			) {
+				console.log(response.data.choices[0].message.content.trim());
 				return response.data.choices[0].message.content.trim();
 			} else {
 				console.error("Unexpected API response:", response.data);
