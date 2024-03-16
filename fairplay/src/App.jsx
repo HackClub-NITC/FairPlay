8e8115b971e7a6c55a6f5c28cc6fc731c5be1eb2
Sourabh kamble <kambleaditya946@gmail.com>
2024-03-16 16:40:47 +0530

    nav

--- fairplay/src/App.jsx
+++ fairplay/src/App.jsx
@@ -4,12 +4,12 @@ import "./App.css";
 import CipherForm from "./components/cipherForm/cipherForm";
 
 import RadioToggle from "./components/RadioToggle/RadioToggle";
-
+import Navbar from "./components/Navbar/Navbar";
 function App() {
 	return (
 		<div className="App">
 
-      <RadioToggle />
+      <Navbar />
 
 		</div>
 	);
