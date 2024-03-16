785e21e388011a78e9d891b9a46b168af39fd9a0
27-aditya <kambleaditya946@gmail.com>
2024-03-17 04:06:46 +0530

    Chatbot Button CSS update by harshini

--- fairplay/src/App.jsx
+++ fairplay/src/App.jsx
@@ -3,15 +3,15 @@ import React from "react";
 import "./App.css";
 import FiveFiveDynamic from "./components/FiveFiveDynamic/FiveFiveDynamic"; // Import the FiveFiveDynamic component
 import ChatbotButton from "./components/ChatBox/ChatBotButton";
+import RadioButton from "./components/RadioButton/RadioButton";
 
 
 function App() {
 	return (
 		<div className="App">
 
-
 		<ChatbotButton />
-		</div>
+	</div>
 	);
 }
 
