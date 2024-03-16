7cabec5285d57636c98561ae20e3606eefdc3e86
Vinit Keshri <vinitkeshri2020@gmail.com>
2024-03-16 15:45:18 +0530

    Adding basic radio pages

--- fairplay/src/App.jsx
+++ fairplay/src/App.jsx
@@ -4,12 +4,14 @@ import "./App.css";
 import CipherForm from "./components/cipherForm";
 import Fivefive from "./components/FiveFive/Fivefive";
 
+import RadioToggle from "./components/RadioToggle/RadioToggle";
+
 function App() {
 	return (
 		<div className="App">
-			<h1>Playfair Cipher</h1>
-			<CipherForm />
-			<br />
+
+      <RadioToggle />
+
 		</div>
 	);
 }
---
+++ fairplay/src/components/RadioDecrypt/RadioDecrypt.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles.css';
+
+const Decrypt = () => {
+  return (
+    <div>
+      <h1>Decrypt</h1>
+    </div>
+  )
+}
+
+export default Decrypt
