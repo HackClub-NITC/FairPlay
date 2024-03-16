8ed606e8ead8fb5b8be7fddf68e7a474c37798c0
27-aditya <kambleaditya946@gmail.com>
2024-03-17 00:14:32 +0530

    Flashing animation in substring done.

--- fairplay/src/components/SubstringDisplay/SubstringDisplay.jsx
+++ fairplay/src/components/SubstringDisplay/SubstringDisplay.jsx
@@ -1,28 +1,35 @@
 import React from "react";
 import styles from "./SubstringDisplay.module.css"; // Import CSS Module
 
-function SubstringDisplay({ substrings, length, first }) {
-	return (
-		<div className={styles.grid_input_text}>
-			{/* Conditionally render based on the 'first' prop */}
-			{first
-				? Array.from({ length }, (_, index) => (
-						<div key={index} className={styles.substring}></div>
-				  ))
-				: substrings.map((substring, index) => (
-						<div key={index} className={styles.substring}>
-							{substring.split("").map((char, charIndex) => (
-								<span
-									key={charIndex}
-									className={char === "x" ? styles.green : styles.black} // Use generated class names
-								>
-									{char}
-								</span>
-							))}
-						</div>
-				  ))}
-		</div>
-	);
+function SubstringDisplay({ substrings, length, first, encrypted }) {
+  return (
+    <div className={styles.grid_input_text}>
+      {/* Conditionally render based on the 'first' prop */}
+      {first ? (
+        Array.from({ length }, (_, index) => (
+          <div key={index} className={styles.substring}></div>
+        ))
+      ) : (
+        substrings.map((substring, index) => (
+          <div
+            key={index}
+            className={`${styles.substring} ${
+              encrypted ? (index === substrings.length - 1 ? styles.pulsate : "") : ""
+            }`}
+          >
+            {substring.split("").map((char, charIndex) => (
+              <span
+                key={charIndex}
+                className={char === "x" ? styles.green : styles.black} // Use generated class names
+              >
+                {char}
+              </span>
+            ))}
+          </div>
+        ))
+      )}
+    </div>
+  );
 }
 
 export default SubstringDisplay;
